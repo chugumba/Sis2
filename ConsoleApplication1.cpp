220eee4088fb74d105d50f2eaf54b8219c7b7508
chugumba <chugumba@rambler.ru>
2023-09-14 13:46:57 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,18 +3,20 @@
 #include <numeric>
 #include <cmath>
 
+using namespace std;
+
 class Rand {
-    std::vector<double> x;
-    std::vector<double> y;
+    vector<double> x;
+    vector<double> y;
 
 public:
-    Rand(std::vector<double> x, std::vector<double> y) : x(x), y(y) {}
+    Rand(vector<double> x, vector<double> y) : x(x), y(y) {}
 
-    double mean(const std::vector<double>& v) {
-        return std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+    double mean(const vector<double>& v) {
+        return accumulate(v.begin(), v.end(), 0.0) / v.size();
     }
 
-    double variance(const std::vector<double>& v) {
+    double variance(const vector<double>& v) {
         double m = mean(v);
         double temp = 0;
         for (double a : v)
@@ -30,21 +32,21 @@ public:
     }
 
     void print() {
-        std::cout << "MO X= " << mean(x) << " Disp X= " << variance(x) << std::endl;
-        std::cout << "MO Y= " << mean(y) << " Disp Y= " << variance(y) << std::endl;
-        std::cout << "MonteKarlo= " << monteCarlo() << std::endl;
+        cout << "MO X= " << mean(x) << " Disp X= " << variance(x) << endl;
+        cout << "MO Y= " << mean(y) << " Disp Y= " << variance(y) << endl;
+        cout << "MonteKarlo= " << monteCarlo() << endl;
     }
 };
 
 int main() {
     int n;
-    std::cin >> n;
+    cin >> n;
 
-    std::vector<double> x(n), y(n);
+    vector<double> x(n), y(n);
     for (int i = 0; i < n; ++i)
-        std::cin >> x[i];
+        cin >> x[i];
     for (int i = 0; i < n; ++i)
-        std::cin >> y[i];
+        cin >> y[i];
 
     Rand r(x, y);
     r.print();
